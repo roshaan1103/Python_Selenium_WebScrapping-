c279a4f6a5ef5165b3c0943f97eadf93137e7419
roshaan1103 <roshaan1103@gmail.com>
2025-04-09 15:49:02 +0500

    Add files via upload

---
+++ Tasl 2.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+driver.get('https://m.cricbuzz.com/cricket-stats/icc-rankings/men/batting')
+driver.maximize_window()
+
+#rows
+rows=driver.find_elements(By.XPATH,"/html/body/div/main/div[2]/div[1]/div/div/div[2]/div") #it was div[2] at the end...remove [2] so that it takes every rows
+rows_count= len(rows)
+rows_count= rows_count-1 # -1 as we have to ignore the headers div it counted
+
+#columns:
+columns=driver.find_elements(By.XPATH,"/html/body/div/main/div[2]/div[1]/div/div/div[2]/div[1]/div")
+columns_count=len(columns)
+
+#Total:
+
+print("Total numbers of rows:",rows_count, "and Total number of columns:",columns_count)
+
+#Task 2 part:
+for i in rows :
+    print(i.text)
+
+time.sleep(2)
+driver.quit()
